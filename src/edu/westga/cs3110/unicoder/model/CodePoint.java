30a7806382a10e6c970ae73252bcd9ae3f6bb788
dwalke22 <70181610+dwalke22@users.noreply.github.com>
2021-09-13 00:22:14 -0400

    Implemented utf-8

--- src/edu/westga/cs3110/unicoder/model/CodePoint.java
+++ src/edu/westga/cs3110/unicoder/model/CodePoint.java
@@ -42,7 +42,28 @@ public class CodePoint {
 	 * @return Encoding hexadecimal string as UTF-8 encoding
 	 */
 	public String toUTF8() {
-		return "";
+		int parsed = Integer.parseUnsignedInt(this.codepoint, 16);
+		if (parsed > 0x007f && parsed < 0x0800) {
+			int upperBits = 0b11000000 + ((parsed & 0b11111000000 >> 6));
+			int lowerBits = 0b10000000 + (parsed & 0b00000111111);
+			return String.format("%04X", (upperBits<< 8) + lowerBits);
+		}
+		else if (parsed > 0x07FF && parsed < 0x10000) {
+			int upperBits = 0b11100000 + ((parsed & 0b1111000000000000) >> 12);
+			int middleBits = 0b10000000 + ((parsed & 0b0000111111000000) >> 6);
+			int lowerBits = 0b10000000 + (parsed & 0b0000000000111111);
+			return String.format("%06X", (upperBits << 16) + (middleBits << 8) + lowerBits);
+		}
+		else if (parsed > 0xFFFF) {
+			int byteOne = 0b11110000 + ((parsed & 0b111000000000000000000) >> 18);
+			int byteTwo = 0b10000000 + ((parsed & 0b000111111000000000000) >> 12);
+			int byteThree = 0b10000000 + ((parsed & 0b000000000111111000000) >> 6);
+			int byteFour = 0b10000000 + (parsed & 0b000000000000000111111);
+			return String.format("%08X", (byteOne << 24) + (byteTwo << 16) + (byteThree << 8) + byteFour);
+		} 
+		else {
+			return String.format("%02X", parsed);
+		} ;
 	}
 	
 	/**
